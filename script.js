3d52bdd56d0bd8352395c6ee33ff0c7c1aebdb5d
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-01-23 21:19:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 
-document.addEventListener("DOMContentLoaded" () => {
-	document.body.innerText = "DOM load success"
-})
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.innerText = "DOM load success";
+});
